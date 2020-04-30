855d3431a757b0f16cae1c4ca1a69084c455d27a
Hyeonki Hong <hhk7734@gmail.com>
2020-04-30 18:06:29 +0900

    init

---
+++ src/index.js
@@ -0,0 +1,74 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2020 Hyeonki Hong <hhk7734@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+const path = require('path');
+
+module.exports = function(context) {
+    const {siteConfig} = context;
+    const {themeConfig} = siteConfig;
+    const {googleAdsense} = themeConfig || {};
+
+    if(!googleAdsense) {
+        throw new Error(
+            `You need to specify 'googleAdsense' object in 'themeConfig' with 'dataAdClinet' field in it to use docusaurus-plugin-google-adsense`,
+          );
+    }
+
+    const {dataAdClient} = googleAdsense;
+
+    if (!dataAdClient) {
+        throw new Error(
+          'You specified the `googleAdsense` object in `themeConfig` but the `dataAdClient` field was missing. ' +
+            'Please ensure this is not a mistake.',
+        );
+      }
+
+    const isProd = process.env.NODE_ENV === 'production';
+
+    return {
+        name: "docusaurus-plugin-google-adsense",
+
+        getClientModules() {
+            return isProd ? [path.resolve(__dirname, './adsense')] : [];
+        },
+
+        injectHtmlTags() {
+            if (!isProd) {
+                return {};
+            }
+            return {
+                headTags: [
+                    {
+                        tagName: "script",
+                        attributes: {
+                            "data-ad-client": dataAdClient,
+                            async: true,
+                            src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+                        }
+                    }
+                ],
+            };
+        },
+    };
+}
